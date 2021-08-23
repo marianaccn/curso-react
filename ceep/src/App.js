426bea8ab6cdc26ff96322ded162650d80b27f0b
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2021-08-23 17:31:40 -0300

    Adicionando components

---
+++ ceep/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ListaDeNotas } from './components/ListaDeNotas';
+
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota"></textarea>
+        <button>Criar nota</button>
+      </form>
+
+     <ListaDeNotas/>
+
+    </section>
+  );
+}
+
+export default App;
---
+++ ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class ListaDeNotas extends Component {
+
+    render() {
+        return (
+        <ul>
+            <li>
+                <section>
+                    <header>
+                        <h3>Título</h3>
+                    </header>
+                </section>
+            </li>
+            <li>
+                <section>
+                    <header>
+                        <p>Escreva sua nota</p>
+                    </header>
+                </section>
+            </li>
+        </ul>);
+    }
+        
+}
