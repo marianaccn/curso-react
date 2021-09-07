40cddff28694fd10737ab51e79d3daa8d375da22
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2021-09-07 11:51:17 -0300

    Adicionando props

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -2,21 +2,35 @@ import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
-import './assets/index.css';
+import "./assets/index.css";
 class App extends Component {
-
-  criarNota(titulo, texto){
-    console.log(`uma nova nota foi criada ` + titulo + " " + texto)
+  constructor(props) {
+    super(props);
+    this.state = {
+      notas: [],
+    };
   }
+
+  criarNota(titulo, texto) {
+    this.setState({
+      notas: [
+        ...this.state.notas,
+        {
+          titulo,
+          texto,
+        },
+      ],
+    });
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota}/>
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
 }
-
 
 export default App;
--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -5,9 +5,9 @@ class CardNota extends Component {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
-          <h3 className="card-nota_titulo">Título</h3>
+          <h3 className="card-nota_titulo">{this.props.categoria.titulo}</h3>
         </header>
-        <p className="card-nota_texto">Escreva sua nota</p>
+        <p className="card-nota_texto">{this.props.categoria.texto}</p>
       </section>
     );
   }
